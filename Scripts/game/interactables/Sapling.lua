f506b58c848d3b4fff50fc31cd33f5702302cb08
Dr-Pixel-Plays <86497424+Dr-Pixel-Plays@users.noreply.github.com>
2022-05-11 23:48:47 +0200

    Saplings

---
+++ Scripts/game/interactables/Sapling.lua
@@ -0,0 +1,156 @@
+dofile("$SURVIVAL_DATA/Scripts/game/survival_loot.lua")
+dofile("$SURVIVAL_DATA/Scripts/game/survival_harvestable.lua")
+
+Sapling = class()
+--hvs_burntforest.json
+Sapling.burned = {	sm.uuid.new("9ef210c0-ea30-4442-a1fe-924b5609b0cc"),
+					sm.uuid.new("2bae67d4-c8ef-4c6e-a1a7-42281d0b7489"),
+					sm.uuid.new("8f7a8108-2712-47b3-bce2-f25315165094"),
+					sm.uuid.new("515aed88-0594-42b6-a352-617e5f5a3e45"),
+					sm.uuid.new("2d5aa53d-eb9c-478c-a70f-c57a43753814"),
+					sm.uuid.new("c08b553a-a917-4e26-bbb6-7b8523789cad"),
+					sm.uuid.new("d3fcfc06-a6b6-4598-99b1-9a6445b976b3"),
+					sm.uuid.new("b5f90719-fbca-4c59-89c3-187cdb5553d4")	}
+
+function Sapling.server_onCreate(self)
+	self.saved = self.storage:load()
+	if self.saved == nil then
+		self.saved = {}
+		self.caculatingBox = true
+		self.saved.burned = false
+	end
+
+	self:server_clientDataUpdate()
+end
+
+function Sapling.client_onCreate(self)
+	self.caculating = true -- default true!
+end
+
+function Sapling.check_ground(self)
+	local valid = false
+	local treePos = sm.vec3.zero()
+	local raycast_start = self.shape.worldPosition + sm.vec3.new(0,0,0.125)
+	local raycast_end = self.shape.worldPosition + sm.vec3.new(0,0,-0.3)
+	local body = sm.shape.getBody(self.shape)
+	local success, result = sm.physics.raycast( raycast_start, raycast_end, body)
+	
+	local isInWater = false
+	if sm.exists(self.boundingBox) then
+		for _, result in ipairs( self.boundingBox:getContents() ) do
+			if sm.exists( result ) then
+				local userData = result:getUserData()
+				if userData and userData.water then
+					isInWater = true
+				end
+			end
+		end
+	end
+
+	if success and result.type == "terrainSurface" and not isInWater  then
+		valid = true
+		treePos = result.pointWorld
+	end
+	return valid, treePos
+end
+
+function Sapling.server_clientDataUpdate( self )
+	self.network:setClientData( { valid = self.saved.valid or false, planted = self.saved.planted or false, caculating = self.caculatingBox } )
+end
+
+function Sapling.client_onClientDataUpdate( self, params )
+	self.caculating = params.caculating
+	self.valid = params.valid
+	self.planted = params.planted
+end
+
+function Sapling.client_canInteract(self)
+	if self.planted then
+		sm.gui.setInteractionText("", "Growing...", "")
+	elseif self.valid then
+		sm.gui.setInteractionText("Splash with", "Liquid", "")
+	elseif not self.caculating then
+		sm.gui.setInteractionText("Place on the", "#ff0000Surface", "")
+	end
+	return true
+end
+
+function Sapling.server_onProjectile( self, hitPos, hitTime, hitVelocity, projectileName, attacker, damage )
+	
+	local chemical = projectileName == "chemical"
+
+	if projectileName == "water" or chemical and not self.planted then
+		local valid, treePos = Sapling.check_ground(self)
+
+		if valid then		
+			sm.effect.playEffect("Cotton - Picked", treePos + sm.vec3.new(0, 0, -0.5))
+			sm.effect.playEffect("Tree - LogAppear", treePos)
+			
+			self.saved.treePos = treePos
+			self.saved.grow = math.random(60, 60*24) --1 to 24 minutes
+			
+			self.saved.planted = true
+			self.saved.burned = chemical
+			self.storage:save( self.saved )
+			self.network:setClientData( { valid = self.saved.valid, planted = self.saved.planted } )
+			
+			-- destroy bounding box
+			if sm.exists(self.boundingBox) then
+				sm.areaTrigger.destroy( self.boundingBox )
+			end
+
+			if chemical then
+				sm.effect.playEffect("Plants - Destroyed", self.shape.worldPosition)
+			else
+				sm.effect.playEffect("Plants - Planted", self.shape.worldPosition)
+			end
+		end
+	end
+end
+
+function Sapling:server_onFixedUpdate()
+	if self.saved and self.planted and sm.game.getCurrentTick() % (40 * 5) == 0 then
+		if self.saved.grow == 0 then
+			local offset = sm.vec3.new(0.375, -0.375, 0)
+			if self.saved.burned then --chemicals lead to ember trees
+				self.trees = self.burned
+			end
+			sm.harvestable.create( self.trees[math.random(#self.trees)], self.saved.treePos - offset, sm.quat.new(0.707, 0, 0, 0.707) )
+			self.shape:destroyPart(0)
+			return
+		end
+		self.saved.grow = math.max(self.saved.grow - 5, 0) -- five seconds per call
+		self.storage:save( self.saved )
+	end
+
+	if self.caculatingBox and sm.game.getCurrentTick() % 6 == 0 then 
+		if self.boundingBox == nil then
+			self.boundingBox = sm.areaTrigger.createAttachedBox( self.interactable, sm.vec3.one() / 4, sm.vec3.zero(), sm.quat.identity(), sm.areaTrigger.filter.areaTrigger )
+			return -- return so box can caculate its contents.
+		end
+
+		self.saved.grow = -1
+		self.saved.valid, self.saved.treePos = Sapling.check_ground(self)
+
+		self.storage:save( self.saved )
+		self.network:setClientData( { valid = self.saved.valid, planted = self.saved.planted } )
+
+		self.caculatingBox = false
+	end	
+end
+
+function Sapling:server_canErase()
+	return not self.planted
+end
+
+BirchSapling = class( Sapling )
+BirchSapling.trees = { hvs_tree_birch01, hvs_tree_birch01, hvs_tree_birch03 }
+
+LeafySapling = class( Sapling )
+LeafySapling.trees = { hvs_tree_leafy01, hvs_tree_leafy02, hvs_tree_leafy03}
+
+SpruceSapling = class( Sapling )
+SpruceSapling.tree = { hvs_tree_spruce01, hvs_tree_spruce02, hvs_tree_spruce03 }
+
+PineSapling = class( Sapling )
+PineSapling.tree = { hvs_tree_pine01, hvs_tree_pine02, hvs_tree_pine03 }
---
+++ Scripts/game/raft_items.lua
@@ -0,0 +1,46 @@
+obj_scrap_field = sm.uuid.new("8e75cddb-040e-4813-8dd6-7e72f3288a91")
+obj_large_field = sm.uuid.new("c80e2932-0d21-48bb-b5a2-dba03d73cfb7")
+obj_raft_base = sm.uuid.new("4a971f7d-14e6-454d-bce8-0879243c4474")
+obj_fins = sm.uuid.new("7ece8b10-5cc0-4791-b75e-09858ef1b9c4")
+obj_oxygen_tank = sm.uuid.new("7ceccadf-010f-4a45-907f-18ccc11c509c")
+obj_airtank_empty = sm.uuid.new("03075a5a-92f5-49bd-b132-7d8e0cb80156")
+obj_airtank_full = sm.uuid.new("462baf58-6329-4f29-8f9e-c20d06fb6f7d")
+obj_scrap_purifier = sm.uuid.new("80c9edb7-0f86-4899-bf60-c3baca3fa841")
+obj_scrap_tree_grower = sm.uuid.new("819fb238-a68b-4aab-a6f2-c1037f6bfb5b")
+
+obj_birchtree_sapling = sm.uuid.new("eefb0967-d21c-445e-8120-0a09422e1374")
+obj_leafytree_sapling = sm.uuid.new("b84e5fbb-97f2-4de5-bc39-4adb325a56f1")
+obj_sprucetree_sapling = sm.uuid.new("01a4d4c6-469a-4d28-8c69-b74fff83fbdf")
+obj_pinetree_sapling = sm.uuid.new("9e815dcf-cfb0-4672-b0f4-34f1e9ecc1a1")
+
+obj_scrap_workbench = sm.uuid.new("f3163e62-e6c3-475c-805e-559f8bb07ad1")
+obj_apiary = sm.uuid.new("ebabda72-94d8-40c5-ab07-7001d1b67380")
+obj_propeller = sm.uuid.new("885513a7-baa3-44ee-aa6e-7546bf0715a7")
+obj_propeller_small = sm.uuid.new("4e69beb8-4519-4fff-a382-fdcfffbad6c2")
+
+obj_wasdConverter = sm.uuid.new("509505f1-b772-4acd-9827-f9a605a979ec")
+obj_waterSensor = sm.uuid.new("6db7aacf-f2b3-4cbb-a6d2-2a01d5ee5562")
+obj_creationSteer = sm.uuid.new("71ac7645-57f5-4d49-9574-c8482b431862")
+obj_sail = sm.uuid.new("eea9794b-bcdf-431e-bdcd-7b2ff6ffa145")
+obj_bee_queen = sm.uuid.new("c2418698-7a91-40fa-95a3-3be5ddb3ec1d")
+obj_radio_antenna = sm.uuid.new("f95beb80-49c8-4d4c-98e9-5b87225c4570")
+obj_seed_press = sm.uuid.new("e9c7d080-ae4b-4fec-b626-34afda6f5114")
+obj_hammack = sm.uuid.new("4c1aed86-a402-4240-81e7-b67edf433fd5")
+
+obj_grill = sm.uuid.new("67dd0023-023d-46cb-9839-e4ada2609a9b")
+obj_flag = sm.uuid.new("8f460344-ef34-42ab-99e3-d3bed9af4b52")
+
+--RAFT Consumables
+obj_water_clean = sm.uuid.new("4bd05d7d-898c-4947-8788-e34b84b5d386")
+obj_fish = sm.uuid.new("7c440c4a-5919-4baa-a946-0bb89b8c4d5d")
+obj_fishus = sm.uuid.new("7c440c4a-5919-4baa-a946-0bb89b8c4d85")
+obj_fish_cooked = sm.uuid.new("c57fe6d6-03b1-46d4-9104-29cf19f176ab")
+obj_arrow = sm.uuid.new("4a971f7d-14e6-454d-bce8-0879243c4857")
+
+--RAFT TOOLS
+tool_fishing_rod = sm.uuid.new("e3bcf8fa-68d7-49a8-b17b-c1e864d53b69")
+tool_hook = sm.uuid.new("b5ade3b0-be48-4a61-a8c2-13a4cedf1343")
+tool_axe = sm.uuid.new("1daebe24-4e16-422c-b5e7-c3f45f9296d8")
+tool_pickaxe = sm.uuid.new("c883b283-ea7a-4bba-af0d-5c13fd73051d")
+tool_spear = sm.uuid.new("0db1ecd5-503d-4dd3-807e-869c243a7c08")
+tool_harpoon = sm.uuid.new("e34387d3-fa29-4831-89b8-c94417b6fce6")
