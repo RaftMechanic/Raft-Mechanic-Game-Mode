21af69fee3802f26752c97934459dac4d21ee375
Dr-Pixel-Plays <86497424+Dr-Pixel-Plays@users.noreply.github.com>
2022-05-12 13:59:11 +0200

    Added a lot of items

--- Scripts/game/raft_items.lua
+++ Scripts/game/raft_items.lua
@@ -22,6 +22,21 @@ obj_planter_broccoli = sm.uuid.new("704c9bb4-747b-4e54-b1f2-d51043929b5b")
 obj_planter_potato = sm.uuid.new("56ac7150-65b2-496d-9c65-0197a99ca571")
 obj_planter_cotton = sm.uuid.new("d9dce2fc-b158-464d-b5a6-163c6e65b512")
 
+--equipment.shapeset
+obj_fins = sm.uuid.new("7ece8b10-5cc0-4791-b75e-09858ef1b9c4")
+obj_oxygen_tank = sm.uuid.new("7ceccadf-010f-4a45-907f-18ccc11c509c")
+
+--parts.shapeset
+obj_raft_base = sm.uuid.new("4a971f7d-14e6-454d-bce8-0879243c4474")
+obj_reinforced_raft_base = sm.uuid.new("4a971f7d-14e6-454d-bce8-0879243c4575")
+obj_bee_queen = sm.uuid.new("c2418698-7a91-40fa-95a3-3be5ddb3ec1d")
+
+--consumables.shapeset
+obj_water_clean = sm.uuid.new("4bd05d7d-898c-4947-8788-e34b84b5d386")
+obj_fish = sm.uuid.new("7c440c4a-5919-4baa-a946-0bb89b8c4d5d")
+obj_fishus = sm.uuid.new("7c440c4a-5919-4baa-a946-0bb89b8c4d85")
+obj_fish_cooked = sm.uuid.new("c57fe6d6-03b1-46d4-9104-29cf19f176ab")
+obj_harpoon = sm.uuid.new("4a971f7d-14e6-454d-bce8-0879243c4857")
 
 
 
@@ -29,9 +44,7 @@ obj_planter_cotton = sm.uuid.new("d9dce2fc-b158-464d-b5a6-163c6e65b512")
 
 obj_scrap_field = sm.uuid.new("8e75cddb-040e-4813-8dd6-7e72f3288a91")
 obj_large_field = sm.uuid.new("c80e2932-0d21-48bb-b5a2-dba03d73cfb7")
-obj_raft_base = sm.uuid.new("4a971f7d-14e6-454d-bce8-0879243c4474")
-obj_fins = sm.uuid.new("7ece8b10-5cc0-4791-b75e-09858ef1b9c4")
-obj_oxygen_tank = sm.uuid.new("7ceccadf-010f-4a45-907f-18ccc11c509c")
+
 obj_airtank_empty = sm.uuid.new("03075a5a-92f5-49bd-b132-7d8e0cb80156")
 obj_airtank_full = sm.uuid.new("462baf58-6329-4f29-8f9e-c20d06fb6f7d")
 obj_scrap_purifier = sm.uuid.new("80c9edb7-0f86-4899-bf60-c3baca3fa841")
@@ -48,7 +61,7 @@ obj_wasdConverter = sm.uuid.new("509505f1-b772-4acd-9827-f9a605a979ec")
 obj_waterSensor = sm.uuid.new("6db7aacf-f2b3-4cbb-a6d2-2a01d5ee5562")
 obj_creationSteer = sm.uuid.new("71ac7645-57f5-4d49-9574-c8482b431862")
 obj_sail = sm.uuid.new("eea9794b-bcdf-431e-bdcd-7b2ff6ffa145")
-obj_bee_queen = sm.uuid.new("c2418698-7a91-40fa-95a3-3be5ddb3ec1d")
+
 obj_radio_antenna = sm.uuid.new("f95beb80-49c8-4d4c-98e9-5b87225c4570")
 obj_seed_press = sm.uuid.new("e9c7d080-ae4b-4fec-b626-34afda6f5114")
 obj_hammack = sm.uuid.new("4c1aed86-a402-4240-81e7-b67edf433fd5")
@@ -57,11 +70,8 @@ obj_grill = sm.uuid.new("67dd0023-023d-46cb-9839-e4ada2609a9b")
 obj_flag = sm.uuid.new("8f460344-ef34-42ab-99e3-d3bed9af4b52")
 
 --RAFT Consumables
-obj_water_clean = sm.uuid.new("4bd05d7d-898c-4947-8788-e34b84b5d386")
-obj_fish = sm.uuid.new("7c440c4a-5919-4baa-a946-0bb89b8c4d5d")
-obj_fishus = sm.uuid.new("7c440c4a-5919-4baa-a946-0bb89b8c4d85")
-obj_fish_cooked = sm.uuid.new("c57fe6d6-03b1-46d4-9104-29cf19f176ab")
-obj_arrow = sm.uuid.new("4a971f7d-14e6-454d-bce8-0879243c4857")
+
+
 
 --RAFT TOOLS
 tool_fishing_rod = sm.uuid.new("e3bcf8fa-68d7-49a8-b17b-c1e864d53b69")
