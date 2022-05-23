8e39fbfd5cf353e9634bd8dff36ee688726ee13e
Tofix26 <73693639+Tofix26@users.noreply.github.com>
2022-05-23 20:23:57 +0200

    pls work

--- Scripts/game/tools/Torch.lua
+++ Scripts/game/tools/Torch.lua
@@ -15,51 +15,49 @@ sm.tool.preloadRenderables(renderablesTp)
 sm.tool.preloadRenderables(renderablesFp)
 
 function TorchTool:client_onEquip()
-    self.equipped = true
-    if not self.effect then
-        self.network:sendToServer("sv_createEffect")
-    end
+	self.equipped = true
+	if not self.effect then
+		self.network:sendToServer("sv_createEffect")
+	end
 end
 
 function TorchTool:client_onUnequip()
-    self.equipped = false
-    self.network:sendToServer("sv_stopEffect")
+	self.equipped = false
+	self.network:sendToServer("sv_stopEffect")
 end
 
----@param player Player
-function TorchTool:sv_createEffect(_, player)
-    self.network:sendToClients("cl_effect", player)
+function TorchTool:sv_createEffect()
+	self.network:sendToClients("cl_effect")
 end
 
----@param player Player
-function TorchTool:cl_effect(player)
-    self.effect = sm.effect.createEffect("Fire - small01", player:getCharacter())
-    self.effect:setOffsetPosition(sm.vec3.new(-1, 1, 0))
+function TorchTool:cl_effect()
+	self.effect = sm.effect.createEffect("Fire - small01", self.tool:getOwner():getCharacter())
+	self.effect:setOffsetPosition(sm.vec3.new(-1, 1, 0))
 end
 
 function TorchTool:sv_startEffect()
-    self.network:sendToClients("cl_startEffect")
+	self.network:sendToClients("cl_startEffect")
 end
 
 function TorchTool:cl_startEffect()
-    if self.effect:isPlaying() then return end
-    self.effect:start()
+	if self.effect:isPlaying() then return end
+	self.effect:start()
 end
 
 function TorchTool:sv_stopEffect()
-    self.network:sendToClients("cl_stopEffect")
+	self.network:sendToClients("cl_stopEffect")
 end
 
 function TorchTool:cl_stopEffect()
-    self.effect:stopImmediate()
+	self.effect:stopImmediate()
 end
 
 function TorchTool:client_onFixedUpdate()
-    if not self.equipped then return end
-    ---@diagnostic disable-next-line: missing-parameter
-    if sm.localPlayer.getPlayer():getCharacter():isSwimming() then
-        self.network:sendToServer("sv_stopEffect")
-    else
-        self.network:sendToServer("sv_startEffect")
-    end
+	if not self.equipped then return end
+	---@diagnostic disable-next-line: missing-parameter
+	if self.tool:getOwner():getCharacter() then
+		self.network:sendToServer("sv_stopEffect")
+	else
+		self.network:sendToServer("sv_startEffect")
+	end
 end
