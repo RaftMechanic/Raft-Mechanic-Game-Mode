3d3239bc96dbb210f705a13c6f7fb2016f645cdf
Dr-Pixel-Plays <86497424+Dr-Pixel-Plays@users.noreply.github.com>
2022-05-31 15:14:11 +0200

    Fixed WindManager?

--- Scripts/game/managers/WindManager.lua
+++ Scripts/game/managers/WindManager.lua
@@ -5,95 +5,43 @@ dofile( "$CONTENT_DATA/Scripts/game/managers/QuestManager.lua" )
 WindManager = class()
 WindManager.isSaveObject = true
 
--- [quest] = sm.vec.new(x, y, z)
-local windMap = { --TODO: add more (order matters) and use callbacks
-    { quest = "quest_radio_location", location = sm.vec3.new(-1820.5, 167.5, 0)},
-    { quest = "quest_find_trader", location = sm.vec3.new(1536, 2048, 0)},
-    { quest = "quest_deliver_vegetables", location = sm.vec3.new(1536, 2048, 0)}
-}
-
-local defaultWindMap = {
-    sm.vec3.new(-4096, -3072, 0),
-    sm.vec3.new(-4096, 3072, 0),
-    sm.vec3.new(4096, -3072, 0),
-    sm.vec3.new(4096, 3072, 0)
+local windMap = {
+    { quest = "quest_radio_location", pos = sm.vec3.new(-1820.5, 167.5, 0)},
+    { quest = "quest_find_trader", pos = sm.vec3.new(1536, 2048, 0)}
 }
 
 function WindManager.server_onCreate(self)
-    self.sv = self.storage:load() 
-        
-    if self.sv == nil then
-        self.sv = {}
-        self.sv.defaultCenter = self:sv_e_randomizeWind(false)
-        self.sv.cachedWind = self:sv_caculateWindCenter()
-
-        self.storage:save(self.sv)
-    end 
-
-    self.network:setClientData(self.sv)
-
-    g_questManager.Sv_SubscribeEvent(QuestEvent.QuestActivated, self.scriptableObject, "sv_e_onQuestChanged")
-    g_questManager.Sv_SubscribeEvent(QuestEvent.QuestCompleted, self.scriptableObject, "sv_e_onQuestChanged")
-    g_questManager.Sv_SubscribeEvent(QuestEvent.QuestAbandoned, self.scriptableObject, "sv_e_onQuestChanged")
-    
-    --recaculate on rejoin, (could fix if game/script crash)
-    g_questManager.Sv_SubscribeEvent(QuestEvent.PlayerJoined, self.scriptableObject, "sv_e_onQuestChanged")
-
-
     g_windManager = self
+
+    self.sv = {}
+    self:sv_e_onQuestChanged()
+
+    g_questManager.Sv_SubscribeEvent(QuestEvent.QuestCompleted, self.scriptableObject, "sv_e_onQuestChanged")
 end
 
 function WindManager:sv_e_onQuestChanged(params)
-    self.sv.cachedWind = self:sv_caculateWindCenter()
-
-    self:sv_syncAndSave()
+    self.sv.center = sm.vec3.zero()
+    for _, wind in ipairs(windMap) do
+        if not QuestManager.Sv_IsQuestComplete( wind.quest ) then
+            self.sv.center = wind.pos
+            break
+        end
+    end
+    print("wind")
+    print(self.sv.center)
+    self.network:setClientData(self.sv)
 end
 
 function WindManager.client_onCreate(self)
     self.cl = {}
-    self.cl.cachedWind = sm.vec3.new(0,0,0)
-    self.cl.defaultCenter = sm.vec3.new(0,0,0)
 
     if not sm.isHost then
         g_windManager = self
     end
 end
 
-function WindManager:sv_syncAndSave()
-    self.storage:save(self.sv)
-    self.network:setClientData(self.sv)
-end
-
 function WindManager.client_onClientDataUpdate( self, data )
-	self.cl.cachedWind = data.cachedWind
-    self.cl.defaultCenter = data.defaultCenter
-end
-
----Get the wind center point based on active quests
----@return Vec3
-function WindManager:sv_caculateWindCenter()
-    for _, data in ipairs(windMap) do
-        local success, completed = pcall(g_questManager.Sv_IsQuestComplete, data.quest)
-
-        if success and not completed then -- failed then assume completed
-            return data.location -- break the loop
-        end
-    end
-
-    return nil
-end
-
----Set default wind
-function WindManager:sv_e_randomizeWind(set)
-    local random = randomStackAmount(1, 2, 4)
-    local windCenter = defaultWindMap[random]
-
-    if set then
-        self.sv.defaultCenter = windCenter
-        self:sv_syncAndSave()
-    end
-
-    return windCenter
+	self.cl.center = data.center
 end
 
 ---GLOBAL functions
@@ -104,21 +52,27 @@ function getWindDir(location)
 
     -- get the center with our custom callback
     local windCenter = getWindCenter()
+    if windCenter then --i.e. we have a quest
+        local windDirection = location - windCenter
+        windDirection.z = 0
 
-    local windDirection = location - windCenter
-    
-    windDirection = windDirection:normalize()
-    windDirection.z = 0
+        return windDirection:normalize()
+    else-- i.e. there are no quests, so CIRCLES around the map
+        local up = sm.vec3.new(0, 0, 1)
+        local dirMiddle = location:normalize()
+        local windDirection = -dirMiddle:cross(up)
+        windDirection.z = 0
 
-    return windDirection
+        return windDirection:normalize()
+    end
 end
 
 ---Get the wind center point cached
 ---@return Vec3
 function getWindCenter()
     if sm.isServerMode() then
-        return g_windManager.sv.cachedWind or g_windManager.sv.defaultCenter
+        return g_windManager.sv.center
     end
 
-    return g_windManager.cl.cachedWind or g_windManager.cl.defaultCenter
+    return g_windManager.cl.center
 end
