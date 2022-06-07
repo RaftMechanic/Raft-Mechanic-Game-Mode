96cc6bc386edcb8260447ec4a2fde89eb96ecb96
TheDartFrog <30552512+TheDartFrog@users.noreply.github.com>
2022-06-07 15:18:01 +0200

    improved recipe loader

--- Scripts/game/RecipeLoader.lua
+++ Scripts/game/RecipeLoader.lua
@@ -1,72 +1,125 @@
-dofile("$CONTENT_40639a2c-bb9f-4d4f-b88c-41bfe264ffa8/Scripts/ModDatabase.lua")
-ModDatabase.loadShapesets()
+-- MIT License
 
-local sv_mod_uuid_table = ModDatabase.getAllLoadedMods()
+-- Copyright (c) 2022 QuestionableM
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+--
+--https://github.com/QuestionableM/Modded-Craftbot-Recipes
+--
+
+
+dofile("$CONTENT_40639a2c-bb9f-4d4f-b88c-41bfe264ffa8/Scripts/ModDatabase.lua")
+
+local function is_uuid_valid(uuid)
+	local s_item = sm.item
+	return s_item.isBlock(uuid) or s_item.isHarvestablePart(uuid) or s_item.isJoint(uuid) or s_item.isPart(uuid) or s_item.isTool(uuid)
+end
 
 local function load_recipes_and_store_in_table(path, out_table)
-	if sm.json.fileExists(path) then
-		local success, json_data = pcall(sm.json.open, path)
-		if success == true then
-			print("Loading modded crafting recipes:", path)
+	local success, file_exists = pcall(sm.json.fileExists, path)
+	if not (success == true and file_exists == true) then
+		return
+	end
 
-			for k, mod_recipe in ipairs(json_data) do
-				out_table[#out_table + 1] = mod_recipe
-			end
+	local success, json_data = pcall(sm.json.open, path)
+	if success ~= true then
+		return
+	end
+
+	local temp_recipe_storage = {}
+
+	local l_table_insert = table.insert
+	local l_uuid_new = sm.uuid.new
+
+	for k, mod_recipe in ipairs(json_data) do
+		if mod_recipe.craftTime == nil then
+			mod_recipe.craftTime = 30
 		end
+
+		local success, item_uuid = pcall(l_uuid_new, mod_recipe.itemId)
+		if success == true and is_uuid_valid(item_uuid) then
+			l_table_insert(temp_recipe_storage, mod_recipe)
+		else
+			sm.log.warning("Found an invalid recipe in: ", path, item_uuid)
+			return
+		end
+	end
+
+	print("[CraftbotRecipes] Successfully loaded crafting recipes from:", path)
+	for k, v in ipairs(temp_recipe_storage) do
+		out_table[#out_table + 1] = v
 	end
 end
 
 --this list contains the mods that already have crafting recipes, but they will most likely not be compatible with the custom game
 local mod_exception_list =
 {
-	["f5452069-d631-422c-a1dc-2957c935cbaa"] =
-	{
-		craftbot = "/CraftingRecipes/nomad_default.json"
-	},
-	["df10d497-a28e-4413-a707-5a07813aec37"] =
+	["df10d497-a28e-4413-a707-5a07813aec37"] = --wings mod
 	{
 		craftbot = "/Survival/CraftingRecipes/craftbot.json"
 	}
 }
 
-local function load_modded_crafting_recipes(out_table, file_name)
-	for k, mod_uuid in ipairs(sv_mod_uuid_table) do
+cmi_merged_recipes_paths =
+{
+	craftbot  = "$CONTENT_DATA/Scripts/MergedRecipes.json",
+	workbench = "$CONTENT_DATA/Scripts/WorkbenchMergedRecipes.json",
+	hideout   = "$CONTENT_DATA/Scripts/HideoutMergedRecipes.json"
+}
+
+function merge_custom_crafting_recipes()
+	ModDatabase.loadDescriptions()
+
+	local craftbot_recipes  = {}
+	local workbench_recipes = {}
+	local hideout_recipes   = {}
+
+	load_recipes_and_store_in_table("$SURVIVAL_DATA/CraftingRecipes/craftbot.json" , craftbot_recipes )
+	load_recipes_and_store_in_table("$SURVIVAL_DATA/CraftingRecipes/workbench.json", workbench_recipes)
+	load_recipes_and_store_in_table("$SURVIVAL_DATA/CraftingRecipes/hideout.json"  , hideout_recipes  )
+
+	for mod_uuid, v in pairs(ModDatabase.databases.descriptions) do
 		local cur_exception = mod_exception_list[mod_uuid]
 		local mod_key = "$CONTENT_"..mod_uuid
 
 		if cur_exception == nil then
-			local full_path = mod_key.."/CraftingRecipes/"..file_name..".json"
+			local recipe_folder = mod_key.."/CraftingRecipes/"
 
-			load_recipes_and_store_in_table(full_path, out_table)
+			load_recipes_and_store_in_table(recipe_folder.."craftbot.json", craftbot_recipes)
+			load_recipes_and_store_in_table(recipe_folder.."workbench.json", workbench_recipes)
+			load_recipes_and_store_in_table(recipe_folder.."hideout.json", hideout_recipes)
 		else
-			local cur_exc_path = cur_exception[file_name]
-			if cur_exc_path ~= nil then
-				local full_path = mod_key..cur_exc_path
+			if cur_exception.craftbot then
+				load_recipes_and_store_in_table(mod_key..cur_exception.craftbot, craftbot_recipes)
+			end
 
-				load_recipes_and_store_in_table(full_path, out_table)
+			if cur_exception.workbench then
+				load_recipes_and_store_in_table(mod_key..cur_exception.workbench, workbench_recipes)
+			end
+
+			if cur_exception.hideout then
+				load_recipes_and_store_in_table(mod_key..cur_exception.hideout, hideout_recipes)
 			end
 		end
 	end
-end
 
-cmi_merged_recipes_paths =
-{
-	craftbot = "$CONTENT_DATA/Scripts/MergedRecipes.json",
-	workbench = "$CONTENT_DATA/Scripts/WorkbenchMergedRecipes.json",
-	hideout = "$CONTENT_DATA/Scripts/HideoutMergedRecipes.json"
-}
-
-local function merge_craftbot_recipes(game_path, name)
-	local merged_recipes = {}
-
-	load_recipes_and_store_in_table(game_path, merged_recipes)
-	load_modded_crafting_recipes(merged_recipes, name)
-
-	sm.json.save(merged_recipes, cmi_merged_recipes_paths[name])
-end
-
-function merge_custom_crafting_recipes()
-	merge_craftbot_recipes("$SURVIVAL_DATA/CraftingRecipes/craftbot.json", "craftbot")
-	merge_craftbot_recipes("$SURVIVAL_DATA/CraftingRecipes/workbench.json", "workbench")
-	merge_craftbot_recipes("$SURVIVAL_DATA/CraftingRecipes/hideout.json", "hideout")
+	sm.json.save(craftbot_recipes, cmi_merged_recipes_paths.craftbot)
+	sm.json.save(workbench_recipes, cmi_merged_recipes_paths.workbench)
+	sm.json.save(hideout_recipes, cmi_merged_recipes_paths.hideout)
 end
