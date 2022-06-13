d858de6662cc7883f9c7812f14d49acdfd3ebeea
Vajdani <dvajda656@gmail.com>
2022-06-13 19:12:24 +0200

    sapling improvement

--- Scripts/game/interactables/Sapling.lua
+++ Scripts/game/interactables/Sapling.lua
@@ -1,5 +1,8 @@
-dofile("$CONTENT_DATA/Scripts/game/managers/LanguageManager.lua")
+--to any pour soul who might stumble upon this script
+--please close it as soon as possible
+--fuck areatrigger bullshit
 
+dofile("$CONTENT_DATA/Scripts/game/managers/LanguageManager.lua")
 dofile("$SURVIVAL_DATA/Scripts/game/survival_loot.lua")
 dofile("$SURVIVAL_DATA/Scripts/game/survival_harvestable.lua")
 
@@ -18,8 +21,6 @@ Sapling.burned = {
 
 function Sapling.server_onCreate(self)
 	self.sv = {}
-	self.sv.boundingBox = nil
-	self.sv.caculatingBox = true
 
 	self.sv.saved = self.storage:load()
 	if self.sv.saved == nil then
@@ -30,53 +31,96 @@ function Sapling.server_onCreate(self)
 		self.sv.saved.valid = false
 	end
 
-	self:server_clientDataUpdate()
-end
-
-function Sapling.client_onCreate(self)
-	self.cl = {}
-	self.cl.caculating = true -- default true!
-	self.cl.valid = false
-	self.cl.planted = false
+	self.sv.tries = 0
+	self.sv.waterTrigger = sm.areaTrigger.createAttachedBox( self.interactable, sm.vec3.one() / 4, sm.vec3.zero(), sm.quat.identity(), sm.areaTrigger.filter.areaTrigger )
+	self.network:setClientData( { valid = self.sv.saved.valid, planted = self.sv.saved.planted } )
 end
 
 function Sapling.sv_checkGround(self)
-	local valid = false
-	local treePos = sm.vec3.zero()
-	local raycast_start = self.shape.worldPosition + sm.vec3.new(0,0,0.125)
-	local raycast_end = self.shape.worldPosition + sm.vec3.new(0,0,-0.3)
-	local body = sm.shape.getBody(self.shape)
-	local success, result = sm.physics.raycast( raycast_start, raycast_end, body)
-
-	local trigger = sm.areaTrigger.createAttachedBox( self.interactable, sm.vec3.one() / 4, sm.vec3.zero(), sm.quat.identity(), sm.areaTrigger.filter.areaTrigger )
-
-	local isInWater = false
-	if sm.exists(trigger) then
-		for _, content in ipairs( trigger:getContents() ) do
-			if sm.exists( content ) then
-				local userData = content:getUserData()
-				if userData and userData.water then
-					isInWater = true
-				end
+	for _, content in ipairs( self.sv.waterTrigger:getContents() ) do
+		if sm.exists( content ) then
+			local userData = content:getUserData()
+			if userData and userData.water then
+				self.sv.saved.valid = false
+				self.sv.saved.treePos = nil
+				self.network:setClientData( { valid = self.sv.saved.valid, planted = self.sv.saved.planted } )
+				return
 			end
 		end
 	end
 
-	sm.areaTrigger.destroy(trigger)
-
-	if success and result.type == "terrainSurface" and not isInWater then
-		valid = true
-		treePos = result.pointWorld
-	end
-	return valid, treePos
+	local success, result = sm.physics.raycast( self.shape.worldPosition + sm.vec3.new(0,0,0.5), self.shape.worldPosition + sm.vec3.new(0,0,-0.5), self.shape.body )
+	self.sv.saved.valid = success and result.type == "terrainSurface"
+	self.sv.saved.treePos = result.pointWorld
+	self.network:setClientData( { valid = self.sv.saved.valid, planted = self.sv.saved.planted } )
 end
 
-function Sapling.server_clientDataUpdate( self )
-	self.network:setClientData( { valid = self.sv.saved.valid or false, planted = self.sv.saved.planted or false, caculating = self.sv.caculatingBox } )
+function Sapling.server_onProjectile( self, hitPos, hitTime, hitVelocity, projectileName, attacker, damage )
+	if not self.sv.saved.valid or self.sv.saved.treePos == nil then return end
+
+	local chemical = projectileName == "chemical"
+
+	if projectileName == "water" or chemical and not self.sv.planted then
+		sm.effect.playEffect("Cotton - Picked", self.sv.saved.treePos + sm.vec3.new(0, 0, -0.5))
+		sm.effect.playEffect("Tree - LogAppear", self.sv.saved.treePos)
+
+		self.sv.saved.grow = math.random(60, 60*24) --1 to 24 minutes
+		self.sv.saved.planted = true
+		self.sv.saved.burned = chemical
+		self.storage:save( self.sv.saved )
+		self.network:setClientData( { valid = self.sv.saved.valid, planted = self.sv.saved.planted } )
+
+		if chemical then
+			sm.effect.playEffect("Plants - Destroyed", self.shape.worldPosition)
+		else
+			sm.effect.playEffect("Plants - Planted", self.shape.worldPosition)
+		end
+	end
+end
+
+function Sapling:server_onFixedUpdate()
+	if not self.sv.saved or not sm.exists(self.sv.waterTrigger) then return end
+
+	local tick = sm.game.getCurrentTick()
+	if not self.sv.saved.valid or self.sv.tries < 10 then
+		if tick % 10 == 0 then
+			self:sv_checkGround()
+			self.sv.tries = self.sv.tries + 1
+		end
+
+		return
+	end
+
+	if self.sv.saved.planted and tick % (40 * 5) == 0 then
+		if self.sv.saved.grow == 0 then
+			local offset = sm.vec3.new(0.375, -0.375, 0)
+			if self.sv.saved.burned then
+				self.trees = self.burned
+			end
+
+			sm.harvestable.create( self.trees[math.random(#self.trees)], self.sv.saved.treePos - offset, sm.quat.new(0.707, 0, 0, 0.707) )
+			self.shape:destroyPart(0)
+			return
+		end
+
+		self.sv.saved.grow = math.max(self.sv.saved.grow - 5, 0)
+		self.storage:save( self.sv.saved )
+	end
+end
+
+function Sapling:server_canErase()
+	return not self.sv.saved.planted
+end
+
+
+
+function Sapling.client_onCreate(self)
+	self.cl = {}
+	self.cl.valid = false
+	self.cl.planted = false
 end
 
 function Sapling.client_onClientDataUpdate( self, params )
-	self.cl.caculating = params.caculating
 	self.cl.valid = params.valid
 	self.cl.planted = params.planted
 end
@@ -87,68 +131,16 @@ function Sapling.client_canInteract(self)
 
 	if self.cl.planted then
 		sm.gui.setInteractionText(o1..language_tag("SaplingGrow")..o2)
-	elseif self.cl.valid then
+		return false
+	end
+
+	if self.cl.valid then
 		sm.gui.setInteractionText(o1..language_tag("SaplingNeedLiquid")..o2)
-	elseif not self.cl.caculating then
+	else
 		sm.gui.setInteractionText(o1..language_tag("SaplingNeedGround")..o2)
 	end
-	return true
-end
 
-function Sapling.server_onProjectile( self, hitPos, hitTime, hitVelocity, projectileName, attacker, damage )
-	local chemical = projectileName == "chemical"
-
-	if projectileName == "water" or chemical and not self.sv.planted then
-		local valid, treePos = Sapling.sv_checkGround(self)
-
-		if valid then
-			sm.effect.playEffect("Cotton - Picked", treePos + sm.vec3.new(0, 0, -0.5))
-			sm.effect.playEffect("Tree - LogAppear", treePos)
-
-			self.sv.saved.treePos = treePos
-			self.sv.saved.grow = math.random(60, 60*24) --1 to 24 minutes
-			self.sv.saved.planted = true
-			self.sv.saved.burned = chemical
-			self.storage:save( self.sv.saved )
-			self.network:setClientData( { valid = self.sv.saved.valid, planted = self.sv.saved.planted } )
-
-			if chemical then
-				sm.effect.playEffect("Plants - Destroyed", self.shape.worldPosition)
-			else
-				sm.effect.playEffect("Plants - Planted", self.shape.worldPosition)
-			end
-		end
-	end
-end
-
-function Sapling:server_onFixedUpdate()
-	if self.sv.saved and self.sv.saved.planted and sm.game.getCurrentTick() % (40 * 5) == 0 then
-		if self.sv.saved.grow == 0 then
-			local offset = sm.vec3.new(0.375, -0.375, 0)
-			if self.sv.saved.burned then --chemicals lead to ember trees
-				self.trees = self.burned
-			end
-			sm.harvestable.create( self.trees[math.random(#self.trees)], self.sv.saved.treePos - offset, sm.quat.new(0.707, 0, 0, 0.707) )
-			self.shape:destroyPart(0)
-			return
-		end
-		self.sv.saved.grow = math.max(self.sv.saved.grow - 5, 0) -- five seconds per call
-		self.storage:save( self.sv.saved )
-	end
-
-	if self.sv.caculatingBox and sm.game.getCurrentTick() % 6 == 0 then
-		self.sv.saved.grow = -1
-		self.sv.saved.valid, self.sv.saved.treePos = Sapling.sv_checkGround(self)
-
-		self.storage:save( self.sv.saved )
-		self.network:setClientData( { valid = self.sv.saved.valid, planted = self.sv.saved.planted } )
-
-		self.sv.caculatingBox = false
-	end
-end
-
-function Sapling:server_canErase()
-	return not self.sv.saved.planted
+	return false
 end
 
 function Sapling:client_canErase()
