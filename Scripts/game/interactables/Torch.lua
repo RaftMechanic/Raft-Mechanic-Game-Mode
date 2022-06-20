45d9645b1b47605ee32445bf96011e7efdbd02e6
Tofix26 <73693639+Tofix26@users.noreply.github.com>
2022-06-20 17:40:56 +0200

    fixed torch

--- Scripts/game/interactables/Torch.lua
+++ Scripts/game/interactables/Torch.lua
@@ -6,13 +6,15 @@ Torch.connectionInput = 1
 Torch.connectionOutput = 1
 Torch.maxParentCount = 1
 Torch.maxChildCount = 1
-
 function Torch:server_onCreate()
-    self.sv = self.storage:load() or { active = false }
-    self.network:setClientData(self.sv)
+    self.network:sendToClients("cl_createEffect")
 end
 
 function Torch:client_onCreate()
+    self:cl_createEffect()
+end
+
+function Torch:cl_createEffect()
     self.effect = sm.effect.createEffect("Fire - small01", self.interactable)
     self.effect:setOffsetPosition(sm.vec3.new(0, 0.35, 0))
     if self.shape.uuid == obj_torch_lit then
@@ -20,48 +22,51 @@ function Torch:client_onCreate()
     end
 end
 
-function Torch.client_canInteract( self, character, state )
+function Torch:client_onInteract(_, state)
+    if not state then return end
+    local parent = self.interactable:getSingleParent()
+    if parent then return end
+    self.network:sendToServer("sv_changeState")
+end
+
+function Torch:sv_changeState()
+    self.shape:replaceShape(self.shape.uuid ~= obj_torch_lit and obj_torch_lit or obj_torch_burnt)
+    self.network:sendToClients("cl_changeState")
+end
+
+function Torch:client_canInteract(character, state)
     local o1 = "<p textShadow='false' bg='gui_keybinds_bg_orange' color='#4f4f4f' spacing='9'>"
     local o2 = "</p>"
     if self.interactable:getSingleParent() then
-        sm.gui.setInteractionText("", o1.."#c60000"..language_tag("Sail_Controlled")..o2 )
+        sm.gui.setInteractionText("", o1 .. "#c60000" .. language_tag("Sail_Controlled") .. o2)
         return false
     end
 
     return true
 end
 
-function Torch:client_onInteract(_, state)
-    if not state then return end
-    self.network:sendToServer("sv_changeState")
-end
-
-function Torch:sv_changeState()
-    if not self.interactable:getSingleParent() then
-        self.sv.active = not self.sv.active
+function Torch:cl_changeState()
+    local parent = self.interactable:getSingleParent()
+    local notBurning = self.shape.uuid ~= obj_torch_lit
+    if parent then
+        notBurning = not notBurning
     end
-
-    self.shape:replaceShape(self.shape.uuid ~= obj_torch_lit and obj_torch_lit or obj_torch_burnt)
-    self.network:setClientData(self.sv)
-    self.storage:save(self.sv)
-end
-
-function Torch:client_onClientDataUpdate( data, channel )
-    if data.active then
+    if notBurning then
         self.effect:start()
     else
-        self.effect:stop()
+        self.effect:stopImmediate()
     end
 end
 
 function Torch:server_onFixedUpdate()
-    local prevActive = self.sv.active
-    local parent = self.interactable:getSingleParent()
-    if parent then
-        self.sv.active = parent:isActive()
+    if not self.effect then
+        return
     end
-
-    if prevActive ~= self.sv.active then
+    local parent = self.interactable:getSingleParent()
+    local isBurining = self.shape.uuid ~= sm.uuid.new("93e8a46a-5c24-42d2-8261-67be84e05ccf")
+    self.interactable:setActive(not isBurining)
+    if not parent then return end
+    if parent:isActive() == isBurining then
         self:sv_changeState()
     end
 end
