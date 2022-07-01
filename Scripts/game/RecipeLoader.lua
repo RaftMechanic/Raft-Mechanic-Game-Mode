306582634b7e365f8c416d827f1fe12b0a293f32
QuestionableM <77170113+QuestionableM@users.noreply.github.com>
2022-07-01 12:41:38 -0700

    Faster recipe loader

--- Scripts/game/RecipeLoader.lua
+++ Scripts/game/RecipeLoader.lua
@@ -24,6 +24,7 @@
 --
 
 dofile("$CONTENT_40639a2c-bb9f-4d4f-b88c-41bfe264ffa8/Scripts/ModDatabase.lua")
+ModDatabase.unloadDescriptions()
 ModDatabase.loadDescriptions()
 
 if not cmi_hideout_trader_storage then
@@ -34,27 +35,30 @@ if not cmi_crafter_object_storage then
 	cmi_crafter_object_storage = {}
 end
 
+local _sm_shape_uuidExists = sm.shape.uuidExists
+local _sm_tool_uuidExists  = sm.tool.uuidExists
 local function is_uuid_valid(uuid)
-	local s_item = sm.item
-	return s_item.isBlock(uuid) or s_item.isHarvestablePart(uuid) or s_item.isJoint(uuid) or s_item.isPart(uuid) or s_item.isTool(uuid)
+	return _sm_shape_uuidExists(uuid) or _sm_tool_uuidExists(uuid)
 end
 
+local _sm_json_open   = sm.json.open
+local _sm_uuid_new    = sm.uuid.new
+local _sm_log_warning = sm.log.warning
 local function is_recipe_file_valid(path, table, uuid_check)
-	local success, json_data = pcall(sm.json.open, path)
+	local success, json_data = pcall(_sm_json_open, path)
 	if success ~= true then
 		return
 	end
 
 	if uuid_check == true then
-		local l_uuid_new = sm.uuid.new
 		for k, mod_recipe in ipairs(json_data) do
 			if mod_recipe.craftTime == nil then
 				mod_recipe.craftTime = 27
 			end
 
-			local success, item_uuid = pcall(l_uuid_new, mod_recipe.itemId)
+			local success, item_uuid = pcall(_sm_uuid_new, mod_recipe.itemId)
 			if not (success == true and is_uuid_valid(item_uuid)) then
-				sm.log.warning("Found an invalid recipe in: ", path, item_uuid)
+				_sm_log_warning("Found an invalid recipe in: ", path, item_uuid)
 				return
 			end
 		end
@@ -92,10 +96,12 @@ local function clean_valid_recipes()
 	cmi_valid_crafting_recipes.hideout   = {}
 end
 
+local _sm_exists = sm.exists
+local _sm_event_sendToInteractable = sm.event.sendToInteractable
 local function cmi_update_crafters(crafter_array, callback)
 	for k, inter in pairs(crafter_array) do
-		if inter and sm.exists(inter) then
-			sm.event.sendToInteractable(inter, callback)
+		if inter and _sm_exists(inter) then
+			_sm_event_sendToInteractable(inter, callback)
 		end
 	end
 end
@@ -105,32 +111,41 @@ function cmi_update_all_crafters()
 	cmi_update_crafters(cmi_crafter_object_storage, "cl_updateRecipeGrid")
 end
 
+local _json_file_exists = sm.json.fileExists
 function initialize_crafting_recipes()
-	local l_craftbot_recipes  = { "$CONTENT_DATA/CraftingRecipes/craftbot.json" }
-	local l_workbench_recipes = { "$CONTENT_DATA/CraftingRecipes/workbench.json" }
-	local l_hideout_recipes   = { "$CONTENT_DATA/CraftingRecipes/hideout.json" }
+	local l_craftbot_recipes  = { "$SURVIVAL_DATA/CraftingRecipes/craftbot.json" }
+	local l_workbench_recipes = { "$SURVIVAL_DATA/CraftingRecipes/workbench.json" }
+	local l_hideout_recipes   = { "$SURVIVAL_DATA/CraftingRecipes/hideout.json" }
  
 	for mod_uuid, v in pairs(ModDatabase.databases.descriptions) do
-		local cur_exception = mod_exception_list[mod_uuid]
 		local mod_key = "$CONTENT_"..mod_uuid
 
-		if cur_exception == nil then
-			local recipe_folder = mod_key.."/CraftingRecipes/"
+		local success, fileExists = pcall(_json_file_exists, mod_key)
+		if success == true and fileExists == true then
+			local cur_exception = mod_exception_list[mod_uuid]
 
-			is_recipe_file_valid(recipe_folder.."craftbot.json" , l_craftbot_recipes )
-			is_recipe_file_valid(recipe_folder.."workbench.json", l_workbench_recipes)
-			is_recipe_file_valid(recipe_folder.."hideout.json"  , l_hideout_recipes  )
-		else
-			if cur_exception.craftbot then
-				is_recipe_file_valid(mod_key..cur_exception.craftbot, l_craftbot_recipes)
-			end
+			if cur_exception == nil then
+				local recipe_folder = mod_key.."/CraftingRecipes/"
 
-			if cur_exception.workbench then
-				is_recipe_file_valid(mod_key..cur_exception.workbench, l_workbench_recipes)
-			end
+				is_recipe_file_valid(recipe_folder.."craftbot.json" , l_craftbot_recipes )
+				is_recipe_file_valid(recipe_folder.."workbench.json", l_workbench_recipes)
+				is_recipe_file_valid(recipe_folder.."hideout.json"  , l_hideout_recipes  )
+			else
+				local exc_craftbot  = cur_exception.craftbot
+				local exc_workbench = cur_exception.workbench
+				local exc_hideout   = cur_exception.hideout
 
-			if cur_exception.hideout then
-				is_recipe_file_valid(mod_key..cur_exception.hideout, l_hideout_recipes)
+				if exc_craftbot then
+					is_recipe_file_valid(mod_key..exc_craftbot, l_craftbot_recipes)
+				end
+
+				if exc_workbench then
+					is_recipe_file_valid(mod_key..exc_workbench, l_workbench_recipes)
+				end
+
+				if exc_hideout then
+					is_recipe_file_valid(mod_key..exc_hideout, l_hideout_recipes)
+				end
 			end
 		end
 	end
